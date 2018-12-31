2c1d6577c408bc3e57efb7c53dcf9239e7c143c3
christine.pham <cstinepham@gmail.com>
2018-12-31 15:09:31 -0800

    Switch order of log in app.js to ensure postman requests work

--- app.js
+++ app.js
@@ -2,8 +2,14 @@ const mongoose = require('mongoose');
 const express = require("express");
 const app = express();
 const bodyParser = require('body-parser');
-
+const User = require('./models/User');
 const db = require('./config/keys').mongoURI;
+
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+app.use(bodyParser.json());
+
 mongoose
   .connect(db, {
     useNewUrlParser: true
@@ -11,7 +17,15 @@ mongoose
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("Ayo, Dawgy Dawg."));
+app.get("/", (req, res) => {
+  const user = new User({
+    handle: "demo",
+    email: "demo@email.com",
+    password: "password"
+  })
+  user.save();
+  res.send("Hello World.");
+});
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 
@@ -21,7 +35,3 @@ const tweets = require("./routes/api/tweets");
 
 app.use("/api/users", users);
 app.use("/api/tweets", tweets);
-app.use(bodyParser.urlencoded({
-  extended: false
-}));
-app.use(bodyParser.json());
--- routes/api/users.js
+++ routes/api/users.js
@@ -7,22 +7,17 @@ const User = require('../../models/User');
 router.get("/test", (req, res) => res.json({
   msg: "This is the users route"
 }));
-
-module.exports = router;
-
 router.post('/register', (req, res) => {
-  // Check to make sure nobody has already registered with a duplicate email
+  
   User.findOne({
       email: req.body.email
     })
     .then(user => {
       if (user) {
-        // Throw a 400 error if the email address already exists
         return res.status(400).json({
           email: "A user has already registered with this address"
         })
       } else {
-        // Otherwise create a new user
         const newUser = new User({
           handle: req.body.handle,
           email: req.body.email,
@@ -41,3 +36,7 @@ router.post('/register', (req, res) => {
       }
     })
 })
+
+
+
+module.exports = router;
