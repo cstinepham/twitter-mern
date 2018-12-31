735bed7025f66efa10e0fc21f1f8c813a7a27011
christine.pham <cstinepham@gmail.com>
2018-12-31 12:56:13 -0800

    Add Users model

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const express = require("express");
 const app = express();
+const bodyParser = require('body-parser');
 
 const db = require('./config/keys').mongoURI;
 mongoose
@@ -9,7 +10,18 @@ mongoose
   })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
-  
+
 app.get("/", (req, res) => res.send("Ayo, Dawgy Dawg."));
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+
+
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+app.use(bodyParser.json());
